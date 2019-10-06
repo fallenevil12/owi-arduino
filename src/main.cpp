646e7168301b404aa30833694f135249d8919ee3
phongbach <ngpbach@gmail.com>
2019-10-06 17:15:07 -0500

    joint 1 pid demo finished

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,41 @@ void setup() {
 
 /** put your main code here, to run repeatedly */
 void loop() {
-//     static L298N joint[5] = { L298N(6, 13, 12),
-//                               L298N(3, 26, 28),
-//                               L298N(4, 22, 24),
-//                               L298N(5, 26, 28),
-//                               L298N(2, 30, 32)  };
+    JOINT::CONFIG joint1conf {
+        .enPin = 3,
+        .dirPin1 = 26,
+        .dirPin2 = 28,
+        .potPin = A0,
+        .degOffset = 40.0,
+        .degMin = 20.0,
+        .degMax = 120,
+        .pid = PID(0.02, 0.001, 0.01)
+    };
 
-//     static POTENTIO pot[2] = { POTENTIO(A0, 10, 5.0, 50),
-//                                POTENTIO(A1, 10, 5.0, 50) };
+    JOINT joint1(joint1conf);
+
+    int target = 0;
+    do {
+        Serial.println("Enter target angle (30 to 110)");
+        while(!Serial.available());
+        target = getInt();
+    } while (target < 30 || target > 110);
+    while (!joint1.driveTo(target)) {
+        delay(10);
+    }
+}
+
+// void motor_dir_test() {
+//     static JOINT joint[5] = { JOINT(6, 13, 12),
+//                               JOINT(3, 26, 28),
+//                               JOINT(4, 22, 24),
+//                               JOINT(5, 26, 28),
 //     Serial.println(pot[0].getDegreeVal());
+
 //     Serial.println(pot[1].getDegreeVal());
 //     oLoopCtrl userInput = manualStep();
 //     joint[userInput.i].update(userInput.direction, userInput.pwm);
 //     joint[userInput.i].setDuration(userInput.duration);
 //     joint[userInput.i].output();
 //     delay(1000);
-    PID pid= PID(0.02, 0.001, 0.02);
-    JOINT joint1 = JOINT(3, 26, 28, A0, pid);
-
-    int target = 130;
-    do {
-        while(!Serial.available());
-        Serial.println("Enter target angle (100-150)");
-        target = getInt();
-    } while (target < 100 || target > 150);
-    while (!joint1.driveTo(target)) {
-        delay(10);
-    }
-}
-
+// }
