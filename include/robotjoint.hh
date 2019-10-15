d4e413685d0763ffc3077b0b13649116f2748c13
bnguye14 <bnguye14@BNbuntuVM.trick.gov>
2019-10-15 15:22:33 -0500

    mend

---
+++ include/robotjoint.hh
@@ -0,0 +1,106 @@
+#ifndef ROBOTJOINT_HH
+#define ROBOTJOINT_HH
+
+#include "motor.hh"
+#include "rotsensor.hh"
+#include "pid.hh"
+#include "SerialHelper.hh"
+
+/** Represent a joint (and coresponding link angle) on OWI robot with control methods */
+class JOINT {
+
+public:
+
+    /** configuration parametters for a joint */
+    struct CONFIG {
+        /** pwm Pin */
+        uint8_t enPin;
+        /** dir pin 1 */
+        uint8_t dirPin1;
+        /** dir pin 2 */
+        uint8_t dirPin2;
+        /** analog in pin */
+        uint8_t potPin;
+        /** angle of defined 0 degree (D-H convention) relative to raw angle */
+        float degOffset;
+        /** safe min angle */
+        float degMin;
+        /** safe max anle */
+        float degMax;
+        /** pid parametters */
+        PID pid;
+    };
+
+    /** 
+     * constructor
+     * @param config configuration parametters for a joint
+    */
+    JOINT(CONFIG config);
+
+    /**
+     * set the target angle, no actuation occur here
+     */
+    void setTarget(float targetAngle);
+
+    /** 
+     * get joint angle value in degree, according to D-H convention
+     */
+    float getAngle();
+
+    /**
+     * safety stop
+     */
+    void halt();
+
+    /**
+     * <WARNING> THIS METHOD HAS TO BE CALLED WITHIN A LOOP </WARNING>
+     * rotate joint to target angle
+     * @return true if reached target or target out of bound
+     */
+    bool actuate();
+
+    /**
+     * for initial testing of correct wiring of motor and sensor
+     * do a small joint movement to see if it is moving in the positive direction of sensor
+     * @return true if the movement cause positive change in sensor reading
+     */
+    bool test_step_pos();
+
+    /**
+     * for initial testing of correct wiring or motor and sensor
+     * do a small joint movement to see if it is moving in the negative direction of sensor
+     * @return true if the movement cause negative change in sensor reading
+     */
+    bool test_step_neg();
+
+    /**
+     * Test pid for a joint
+     */
+    void pid_test();
+
+
+private:
+
+    /* motor driver */
+    L298N driver;
+    /* potentiometer driver */
+    POTENTIO pot;
+    /* pid */
+    PID pid;
+    /* position of 0 degree (D-H convention) relative to raw angle  */
+    float offset;
+    /* operating angle lower bound */
+    float safemin;
+    /* operating angle upper bound */
+    float safemax;
+    /* corresponding link angle */
+    float angle;
+    /* target angle */
+    float target;
+
+}; //class JOINT
+
+
+
+
+#endif //ROBOTJOINT_HH
---
+++ src/robotjoint.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+#include "robotjoint.hh"
+#include "SerialHelper.hh"
+
+#define ANALOG_BITS 10
+#define ANALOG_RANGE 5.0
+#define VOLT2DEG_SCALE 50.0
+#define ACCURACY 1.0
+#define MAXPWM 255
+
+JOINT::JOINT(CONFIG config):
+    driver(config.enPin, config.dirPin1, config.dirPin2), 
+    pot(config.potPin, ANALOG_BITS, ANALOG_RANGE, VOLT2DEG_SCALE),
+    pid(config.pid),
+    offset(config.degOffset),
+    safemin(config.degMin),
+    safemax(config.degMax),
+    target((safemin + safemax)/2) {}
+
+void JOINT::setTarget(float targetAngle) {
+    target = targetAngle;
+}
+
+float JOINT::getAngle() {
+    angle = pot.getDegreeVal() - offset;
+
+    if (angle < safemin || angle > safemax) {
+        halt();
+        char buffer[100];
+        sprintf(buffer, "\n!!!Critical: Joint angle (%d) out of safety bound. Operation halted.\n", static_cast<int>(angle));
+        adnoserial.println(buffer);
+        while(1);
+    }
+
+    return angle;
+}
+
+void JOINT::halt() {
+    driver.setDuration(-1);
+    driver.setPower(0);
+    driver.output();
+}
+bool JOINT::actuate() {
+    getAngle();
+
+    if (target < safemin || target > safemax) {
+        halt();
+        char buffer[100];
+        sprintf(buffer, "\n!!!Warning: Target %d out of safety bound. Halt movement!\n", static_cast<int>(target));
+        adnoserial.println(buffer);
+        return true;
+    }
+
+    float k = pid.pidCal(angle, target);
+
+    if (k < 0) {
+        k = fabs(k);
+        driver.setDirection(MOTOR::dir::CW);
+    } else {
+        driver.setDirection(MOTOR::dir::CCW);
+    }
+    driver.setDuration(-1);
+    driver.setPower(k*MAXPWM);
+    driver.output();
+    
+    if (k == 0.0) return true; // expecting 0.0 value that pidCal would return if reached setpoint
+    else return false;
+}
+
+bool JOINT::test_step_pos() {
+    angle = pot.getDegreeVal();
+    driver.setDirection(MOTOR::dir::CW);
+    driver.setDuration(500);
+    driver.setPower(200);
+    driver.output();
+
+    delay(500);
+    if (pot.getDegreeVal() - angle > 3.0) return true;
+    else return false;
+}
+
+bool JOINT::test_step_neg() {
+    angle = pot.getDegreeVal();
+    driver.setDirection(MOTOR::dir::CCW);
+    driver.setDuration(500);
+    driver.setPower(200);
+    driver.output();
+
+    delay(500);
+    if (pot.getDegreeVal() - angle < -3.0) return true;
+    else return false;
+}
+
+void JOINT::pid_test() {
+    char buffer[100];
+    sprintf(buffer, "Enter target angle degree (%d to %d)", static_cast<int>(safemin), static_cast<int>(safemax));
+
+    do {
+        adnoserial.println(buffer);
+        target = getInt();
+    } while (target < safemin || target > safemax);
+
+    while (!actuate()) {
+        adnoserial.println(angle);
+    }
+}
+
+
+
